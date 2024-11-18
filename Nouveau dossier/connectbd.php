257248621e52b636b8a77b0c6c655e1f748a3b21
DEF313 <maximedefa@gmail.com>
2024-11-18 22:36:58 +0000

    Supprimer le dossier inutilisé

--- Nouveau dossier/connectbd.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-
-    $pdo = new PDO("mysql:host=localhost;dbname=Boutique", "root", "");
-}catch(Exception $error){
-    die('Une Erreur est survenu : '.$error->getMessage());
-}
-
-?>
--- Nouveau dossier/msg.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('connectbd.php');
-
-$comment = $_POST['comment'];
-
-$stmt = $pdo->prepare("INSERT INTO comments (comment) VALUES (:comment)");
-$stmt->bindParam(':comment', $comment);
-$stmt->execute();
